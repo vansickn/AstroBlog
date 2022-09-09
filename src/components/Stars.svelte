28b0e68098160b0321e618692d82a6ee84162fdd
Noah VanSickle <61751706+vansickn@users.noreply.github.com>
2022-09-09 16:46:12 -0400

    new component for random stars + parallax

---
+++ src/components/Stars.svelte
@@ -0,0 +1,116 @@
+<script>
+
+    let scroll;
+    
+    // function goTop(){
+    //     document.body.scrollIntoView();
+    // }
+    // goTop();
+    // need to scroll to the top on every time load
+</script>
+
+<svelte:window bind:scrollY={scroll}/>
+
+
+<div class="starBack" style:left={20}% style:top={20}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+<div class="starBack" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .1}px,0)`}></div>
+
+
+<div class="starMiddle" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .25}px,0)`}></div>
+<div class="starMiddle" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .25}px,0)`}></div>
+<div class="starMiddle" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .25}px,0)`}></div>
+<div class="starMiddle" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .25}px,0)`}></div>
+<div class="starMiddle" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .25}px,0)`}></div>
+<div class="starMiddle" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .25}px,0)`}></div>
+<div class="starMiddle" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .25}px,0)`}></div>
+<div class="starMiddle" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .25}px,0)`}></div>
+<div class="starMiddle" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .25}px,0)`}></div>
+<div class="starMiddle" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .25}px,0)`}></div>
+<div class="starMiddle" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .25}px,0)`}></div>
+<div class="starMiddle" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .25}px,0)`}></div>
+<div class="starMiddle" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .25}px,0)`}></div>
+<div class="starMiddle" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .25}px,0)`}></div>
+<div class="starMiddle" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .25}px,0)`}></div>
+
+
+<div class="starFront" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .4}px,0)`}></div>
+<div class="starFront" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .4}px,0)`}></div>
+<div class="starFront" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .4}px,0)`}></div>
+<div class="starFront" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .4}px,0)`}></div>
+<div class="starFront" style:left={Math.random()*100}% style:top={Math.random()*80}% style:transform={`translate3d(0,${scroll * .4}px,0)`}></div>
+
+
+
+<style>
+    .starBack {
+        position:absolute;
+        width:5px;
+        height:5px;
+        background:white;
+        border-radius: 100%;
+        z-index: -3;
+    }
+    .starMiddle {
+        position:absolute;
+        width:8px;
+        height:8px;
+        background:white;
+        border-radius: 100%;
+        z-index: -2;
+    }
+    .starFront {
+        position:absolute;
+        width:11px;
+        height:11px;
+        background:white;
+        border-radius: 100%;
+        z-index: -1;
+    }
+</style>
