2dc6a10707dfe73cfcfce9df921470cc01f63016
Noah VanSickle <61751706+vansickn@users.noreply.github.com>
2022-09-26 23:44:16 -0400

    functionality for scroll + Clickable animated ui in rive animation

--- src/components/aboutMeAnalysis.svelte
+++ src/components/aboutMeAnalysis.svelte
@@ -4,12 +4,31 @@
     // import aboutMeAnimation from '.../public/portfolio_animations.riv';
 
     let canvas;
-
+    let scroll;
     let canvasWidth = 4000;
     let canvasHeight = 2000;
 
+    let hoveringOnVisit = false;
+
+    function handleClick(){
+        console.log("Function Fired");
+        if(hoveringOnVisit){
+            console.log("Can travel");
+            location.href = "/about";
+        }else{
+            console.log("Cannot Travel");
+        }
+    }
+
+    function handleScroll(){
+        console.log(scroll);
+        console.log("onScroll");
+    }
+
+    // initialize r before I set it in the onMount function. Then, can access.
+    var r = null;
     onMount(()=>{
-        const r = new Rive({
+        r = new Rive({
             src: "/aboutMeAnimation.riv",
             canvas: canvas,
             autoplay: true,
@@ -17,20 +36,43 @@
             onLoad: (_) => {
                 const inputs = r.stateMachineInputs('State Machine 1');
                 const onVisible = inputs.find(i => i.name === 'onVisible');
-                console.log(onVisible);
-                onVisible.value = true;
+                const onVisitedHover = inputs.find(i => i.name === 'onVisitPlanetHover');
+                console.log(onVisitedHover);
+                // onVisible.value = true;
+            },
+            onStateChange: (_) => {
+                console.log("Changed State");
+                const inputs = r.stateMachineInputs('State Machine 1');
+                const onVisitedHover = inputs.find(i => i.name === 'onVisitPlanetHover');
+                if(onVisitedHover.value){
+                    hoveringOnVisit = true;
+                    document.getElementById("retina-canvas").style.cursor = 'pointer';
+                }else{
+                    hoveringOnVisit = false;
+                    document.getElementById("retina-canvas").style.cursor = 'default';
+                }
+
             }
         });
-        if(r){
-            console.log(r);
-        }
     })
+
+    $: if (scroll > 400) {
+        // 400 is a default value for now, in the future need to make calculate exact point.
+        console.log(scroll);
+        const inputs = r.stateMachineInputs('State Machine 1');
+        const onVisible = inputs.find(i => i.name === 'onVisible');
+        // start onVisible animation
+        onVisible.value = true;
+    }
 
    
 
 </script>
 
-<canvas id="retina-canvas" bind:this={canvas} width={canvasWidth} height={canvasHeight}></canvas>
+
+<svelte:window bind:scrollY={scroll}/>
+
+<canvas id="retina-canvas" on:scroll={handleScroll} on:click={handleClick} bind:this={canvas} width={canvasWidth} height={canvasHeight}></canvas>
 
 <style>
 
