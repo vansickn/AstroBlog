781d97c2f155e7ad6cc738636235d8efdcfb6b15
Noah VanSickle <61751706+vansickn@users.noreply.github.com>
2022-10-19 17:29:02 -0400

    changes to blog post + new blog post param

--- src/components/BlogCard.svelte
+++ src/components/BlogCard.svelte
@@ -5,17 +5,31 @@
     // export let date;
     export let heroImage;
     export let description;
+    export let characterCount;
+
+    function lengthOfPostInMinutes(){
+        console.log(characterCount);
+        if(characterCount != null){
+            return Math.ceil(characterCount / 265);
+        }
+        else {
+            return null
+        }
+    }
 
 
 </script>
 
 
-<a href={url} class="container border-gray-800 border-2 rounded-xl bg-gradient-to-br from-gray-900 bg-gray-800 no-underline flex flex-row hover:scale-105 transition duration-500 w-full sm:w-3/4">
+<a href={url} class="grid grid-cols-1 md:grid-cols-2 border-gray-800 border-2 rounded-xl bg-gradient-to-br from-gray-900 bg-gray-800 hover:scale-105 transition duration-500 w-full lg:w-4/6 sm:pb-0 pb-4 overflow-hidden">
 <!-- <div class="container border-gray-800 border-2 rounded-xl bg-gradient-to-br from-gray-900 bg-gray-800 no-underline flex flex-row hover:scale-105 transition transition-duration-1000 w-full sm:w-3/4"> -->
-        <img src={heroImage} class="w-1/2 m-0 rounded-xl object-cover" alt="">
+        <img src={heroImage} class="m-0 rounded-xl object-cover" alt="">
         <div class="container flex flex-col ">
-            <h4 id="post-title" class='m-0 ml-4 mt-4 text-lg sm:text-2xl'>{title}</h4>
-            <p id="post-title" class="m-0 ml-4 mt-1 text-gray-400 text-xs sm:text-base hidden sm:inline">{description}</p>
+            <h4 id="post-title" class='m-0 ml-4 mr-4 mt-4 text-lg sm:text-2xl'>{title}</h4>
+            {#if characterCount != null}
+            <h2 id="post-title" class='m-0 ml-4 mr-4 sm:text-sm text-xs text-gray-400'>{lengthOfPostInMinutes()} minute read</h2>
+            {/if}
+            <p id="post-title" class="m-0 ml-4 mr-4 mt-4 text-sm md:text-base sm:text-lg">{description}</p>
         </div>
     <!-- </div> -->
 </a>
--- src/pages/blog/not-all-at-once.md
+++ src/pages/blog/not-all-at-once.md
@@ -1,10 +1,13 @@
 ---
 layout: "../../layouts/BlogPost.astro"
 title: "Don't do it all at once"
-description: "A lot of people try and figure it out by changing their life in a couple days. Take things slow, do one at a time, and you will achieve so much more."
+description: "A lot of people try and figure it out by changing their life in a couple days. Take things slow, do one at a time."
 pubDate: "October 17 2022"
 heroImage: "/motivation.jpeg"
+characterCount: 1000
 ---
+
+This is an example post, we want to understand how many characters in the blogpost.
 
 ```html
 <div>This is a block of code</div>
