f5c1c7f55b7799212542a8afc9a87d2ac2c72b8f
Noah VanSickle <61751706+vansickn@users.noreply.github.com>
2022-10-18 21:31:06 -0400

    changing blog card to be more complete, still wip

--- src/components/BlogCard.svelte
+++ src/components/BlogCard.svelte
@@ -3,20 +3,21 @@
     export let title;
     export let url;
     // export let date;
-    // export let image;
+    export let heroImage;
+    export let description;
 
 
 </script>
 
 
-<a href={url}>
-<div class="container border-grey-200 border-2 rounded-xl w-full bg-grey-900 no-underline">
-
-        <h2 id="post-title" class='m-0 mt-2 mb-4 ml-3 no-underline'>{title}</h2>
-
-        
-        
-    </div>
+<a href={url} class="container border-gray-800 border-2 rounded-xl bg-gradient-to-br from-gray-900 bg-gray-800 no-underline flex flex-row hover:scale-105 transition duration-500 w-full sm:w-3/4">
+<!-- <div class="container border-gray-800 border-2 rounded-xl bg-gradient-to-br from-gray-900 bg-gray-800 no-underline flex flex-row hover:scale-105 transition transition-duration-1000 w-full sm:w-3/4"> -->
+        <img src={heroImage} class="w-1/2 m-0 rounded-xl object-cover" alt="">
+        <div class="container flex flex-col ">
+            <h4 id="post-title" class='m-0 ml-4 mt-4 text-lg sm:text-2xl'>{title}</h4>
+            <p id="post-title" class="m-0 ml-4 mt-1 text-gray-400 text-xs sm:text-base hidden sm:inline">{description}</p>
+        </div>
+    <!-- </div> -->
 </a>
 
 
@@ -30,10 +31,9 @@
 
 		#post-title {
 			font-family: FuturaBold;
-			animation: slideFromTopFade;
-			animation-duration: 800ms;
-			animation-timing-function: ease-in-out;
-            text-decoration-line: none;
 		}
+    a:link {
+        text-decoration: none;
+    }
 
 </style>
