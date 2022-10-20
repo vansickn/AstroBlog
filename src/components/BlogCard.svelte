9679647916ad15ed7a9bc5d480a06af9a63dd6e5
Noah VanSickle <61751706+vansickn@users.noreply.github.com>
2022-10-19 21:41:55 -0400

    changed to flex instead of grid, and object cover, resizes appropriately now

--- src/components/BlogCard.svelte
+++ src/components/BlogCard.svelte
@@ -21,9 +21,9 @@
 </script>
 
 
-<a href={url} class="grid grid-cols-1 md:grid-cols-2 border-gray-800 border-2 rounded-xl bg-gradient-to-br from-gray-900 bg-gray-800 hover:scale-105 transition duration-500 w-full lg:w-4/6 sm:pb-0 pb-4 overflow-hidden">
+<a href={url} class="container flex flex-col md:flex-row md:grid-cols-2 border-gray-800 border-2 rounded-xl bg-gradient-to-br from-gray-900 bg-gray-800 hover:scale-105 transition duration-500 w-full lg:w-4/6 sm:pb-0 pb-4 overflow-hidden">
 <!-- <div class="container border-gray-800 border-2 rounded-xl bg-gradient-to-br from-gray-900 bg-gray-800 no-underline flex flex-row hover:scale-105 transition transition-duration-1000 w-full sm:w-3/4"> -->
-        <img src={heroImage} class="m-0 rounded-xl object-cover" alt="">
+        <img src={heroImage} class="md:w-1/2 w-full m-0 rounded-xl object-cover" alt="">
         <div class="container flex flex-col ">
             <h4 id="post-title" class='m-0 ml-4 mr-4 mt-4 text-lg sm:text-2xl'>{title}</h4>
             {#if characterCount != null}
