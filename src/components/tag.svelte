5674e17c2664ba9263f4b77a821d108d8a6a86a2
Noah VanSickle <61751706+vansickn@users.noreply.github.com>
2022-10-21 22:40:23 -0400

    tag component

---
+++ src/components/tag.svelte
@@ -0,0 +1,13 @@
+<script>
+
+    export let title;
+
+</script>
+
+<div class="rounded-lg bg-gradient-to-br from-gray-900 to-gray-900  text-gray-400 pl-2 pr-2 cursor-pointer">
+    <p class='text-center m-0'>{title}</p>
+</div>
+
+<style>
+
+</style>
