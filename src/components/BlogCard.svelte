928e9e850975f9fd760d041a90d69c42b9cc6269
Noah VanSickle <61751706+vansickn@users.noreply.github.com>
2022-10-22 21:07:09 -0400

    add shadow to blog card

--- src/components/BlogCard.svelte
+++ src/components/BlogCard.svelte
@@ -61,7 +61,7 @@
     console.log(calculateMaxHeight);
 
     let heroImageSyle = `${calculateImageWidth()} m-0 object-cover`;
-    let anchorContainerStyle = `${calculateFlexDirection()} border-gray-800 border-2 rounded-xl bg-gradient-to-br from-gray-900 bg-gray-800 hover:scale-105 transition duration-500 sm:pb-0 pb-4 overflow-hidden ${calculateMaxHeight()} ${spanStyle}`;
+    let anchorContainerStyle = `${calculateFlexDirection()} border-gray-800 border-2 rounded-xl bg-gradient-to-br from-gray-900 bg-gray-800 hover:scale-105 transition duration-500 sm:pb-0 pb-4 overflow-hidden shadow-xl ${calculateMaxHeight()} ${spanStyle}`;
     
 
 
